6dbbba61e0b00abd7bc1e3bb5822cb09ee9fbb7e
Luqman Olawin <luqmanolawin@Luqmans-MBP.attlocal.net>
2024-02-09 16:46:14 -0500

    first commit

---
+++ sample2.py
@@ -0,0 +1,9 @@
+"""This file is a sample file for the project"""
+def add(number1, number2):
+    """This function should add 2 numbers"""
+    return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}")
